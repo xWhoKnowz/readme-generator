8f83422861b947a12c24b30495ebe7bfd8f57233
xwhoknowz <jeremiah2498@gmail.com>
2024-01-18 21:11:00 -0600

    added readme template, array of objects for user prompts, and logic to generate the readme.md file

--- index.js
+++ index.js
@@ -1,13 +1,97 @@
 // TODO: Include packages needed for this application
+const fs = require(`fs`)
+const inquirer = require(`inquirer`)
+const generateMarkdown = require(`./utils/generateMarkdown`)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// TODO pass questions array of objects into inquirer .prompt() method.
+
+const userPrompts = [
+    {
+        type: `input`,
+        message: `What is your project's name?`,
+        name: `title`,
+    },
+    {
+        type: `input`,
+        message: `Please write your project's description.`,
+        name: `description`,
+    },
+    {
+        type: `input`,
+        message: `What are the installation requirements for your application? Enter 'N/A' if there are none.`,
+        name: `install`,
+    },
+    {
+        type: `input`,
+        message: `What does a user need to know about using your application?`,
+        name: `usage`,
+    },
+    {
+        type: `input`,
+        message: `Who would you like to credit for this project? Enter 'N/A' if you had no collaborators.`,
+        name: `credits`,
+    },
+    {
+        type: `input`,
+        message: `What kind of license would you like to use?`,
+        name: `license`,
+    },
+    {
+        type: `input`,
+        message: ``,
+        name: `badges`,
+    },
+    {
+        type: `input`,
+        message: `What, if any, were the prominent features of this project? Enter N/A if there are none.`,
+        name: `features`,
+    },
+    {
+        type: `input`,
+        message: `What does a user need to know about collaborating on this project?`,
+        name: `collabs`,
+    },
+    {
+        type: `input`,
+        message: `What is the alt text for the project screenshot?`,
+        name: `alt`,
+    },
+    {
+        type: `input`,
+        message: `What is the file path for the project screenshot?`,
+        name: `screenshot`,
+    },
+];
+
+// inquirer
+//     .prompt(userPrompts)
+//     .then((readmeContent) => readmeContent)
+
+
+// TODO: Destructure Array of objects for easier variable access
+// const [title, description, install, usage, credits, license, badges, features, collabs, screenshot] = questions
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    inquirer
+        .prompt(userPrompts)
+        .then((readmeData) => {
 
+            const readmeContent = generateMarkdown(readmeData)
+
+            fs.writeFile(`readme.md`, readmeContent, (err) =>
+                err ? console.log(err) : console.log(`Success!`));
+
+        })
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() { 
+    writeToFile()
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+function generateMarkdown({ title, description, install, usage, credits, license, badges, features, collabs, alt, screenshot }) {
+  return `
+  # ${title}
+
+## Description
+
+${description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+- [Features](#features)
+- [Contributing](#contributing)
+- [Screenshot](#screenshot)
+
+## Installation
+
+${install}
+
+## Usage
+
+${usage}
+
+
+## Credits
+
+${credits}
+
+## License
+
+${license}
+
+---
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+${features}
+
+## How to Contribute
+
+${collabs} 
+
+The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard.
+
+## Webpage Screenshot
+    
+![${alt}](${screenshot})`;
 }
 
 module.exports = generateMarkdown;
